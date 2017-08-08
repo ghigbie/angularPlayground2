9f6afa19903c530bbe62c1f261cb2e84a09c874e
George Higbie <gehigbie@gmail.com>
2017-08-08 16:32:41 +0000

    Add aditional landing page

--- modules/meanApp.js
+++ modules/meanApp.js
@@ -1,6 +1,13 @@
 /* global angular */
 
 (function(){
-    var meanApp = angular.module('meanApp', []);
+    var meanApp = angular.module('meanApp', ['ngRoute']);
     
+    meanApp.config(function ($routeProvider){
+        $routeProvider
+            .when('/', {
+                template: '<h1>This is the MAIN page</h1>'})
+            .when('/about', {
+                template: '<h1>This is the ABOUT page</h1>'});
+    });
 }());
