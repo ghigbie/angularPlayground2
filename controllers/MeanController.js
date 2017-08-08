a344e29e2102c074bd687a6f98810aec63a494a8
George Higbie <gehigbie@gmail.com>
2017-08-08 14:15:04 +0000

    Add functionality to buttons

--- controllers/MeanController.js
+++ controllers/MeanController.js
@@ -3,6 +3,15 @@
 (function(){
     var MeanController = function($scope){
         $scope.name = 'Bill';
+        $scope.number = 0;
+        
+        $scope.increment = function(){
+            $scope.number = $scope.number + 1;
+        };
+        
+        $scope.decrement = function(){
+            $scope.number = $scope.number -1;
+        };
     };
     
     MeanController.$inject = ['$scope'];
